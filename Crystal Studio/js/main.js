fc74f36d0b0145a141670c4447a75852ea7d9c2a
wantsomemore <74734511+wantsomemore@users.noreply.github.com>
2020-12-02 14:40:00 +0200

    pet-project
    pet-project

---
+++ Crystal Studio/js/main.js
@@ -0,0 +1,12 @@
+$(function() {
+$('.slider_inner,.news_slider-inner').slick({
+  nextArrow:'<button type="button" class="slick-btn slick-next"></button>',
+  prevArrow:'<button type="button" class="slick-btn slick-prev"></button>',
+  infinite: false
+});
+
+$('select').styler();
+$('.header_btn-menu').on('click',function(){
+  $('.menu ul').slideToggle();
+});
+} );
